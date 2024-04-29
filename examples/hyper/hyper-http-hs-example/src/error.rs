150c345f29383c23981b3f5ebecc4c994ef38d5f
Antonio Cheong <acheong@duti.dev>
2024-04-29 11:51:57 +0100

    feat: Working socks5 proxy example (no auth)

---
+++ examples/hyper/hyper-http-hs-example/src/error.rs
@@ -0,0 +1,46 @@
+use std::fmt::{Display, Formatter};
+use std::str::Utf8Error;
+use std::string::FromUtf8Error;
+use std::{error, fmt, io, result};
+
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidVersion,
+    AddrUnsupported,
+    CommandUnsupported,
+    Io(io::Error),
+    Utf8(Utf8Error),
+}
+
+impl error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidVersion => write!(f, "invalid protocol version"),
+            Self::AddrUnsupported => write!(f, "address type unsupported"),
+            Self::CommandUnsupported => write!(f, "command unsupported"),
+            Self::Io(e) => e.fmt(f),
+            Self::Utf8(e) => e.fmt(f),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+impl From<Utf8Error> for Error {
+    fn from(e: Utf8Error) -> Self {
+        Self::Utf8(e)
+    }
+}
+
+impl From<FromUtf8Error> for Error {
+    fn from(e: FromUtf8Error) -> Self {
+        Self::Utf8(e.utf8_error())
+    }
+}
--- examples/hyper/hyper-http-hs-example/src/main.rs
+++ examples/hyper/hyper-http-hs-example/src/main.rs
@@ -1,20 +1,23 @@
+mod error;
+mod socks;
 use std::sync::Arc;
 
 use anyhow::Result;
-use futures::StreamExt;
+use arti_client::{DataStream, TorClient, TorClientConfig};
+use futures::{AsyncWriteExt, StreamExt};
 use hyper::body::Incoming;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
-use tokio_util::sync::CancellationToken;
-
-use arti_client::{TorClient, TorClientConfig};
 use safelog::sensitive;
+use tokio_util::sync::CancellationToken;
 use tor_cell::relaycell::msg::Connected;
 use tor_hsservice::config::OnionServiceConfigBuilder;
 use tor_hsservice::StreamRequest;
 use tor_proto::stream::IncomingStreamRequest;
+
+use tokio::io::AsyncReadExt;
 
 struct WebHandler {
     shutdown: CancellationToken,
@@ -57,10 +60,7 @@ async fn main() {
     // The defaults provided are the same as the Arti standalone application, and save data
     // to a conventional place depending on operating system (for example, ~/.local/share/arti
     // on Linux platforms)
-    let config = TorClientConfig::builder()
-        .persistent_state(false)
-        .build()
-        .unwrap();
+    let config = TorClientConfig::default();
 
     // We now let the Arti client start and bootstrap a connection to the network.
     // (This takes a while to gather the necessary consensus state, etc.)
@@ -71,6 +71,11 @@ async fn main() {
         .build()
         .unwrap();
     let (service, request_stream) = client.launch_onion_service(svc_cfg).unwrap();
+    // Show the onion address of the service
+    println!(
+        "onion service address: {}",
+        service.onion_name().unwrap().to_string()
+    );
 
     eprintln!("ready to serve connections");
 
@@ -104,18 +109,62 @@ async fn handle_stream_request(
     handler: Arc<WebHandler>,
 ) -> Result<()> {
     match stream_request.request() {
-        IncomingStreamRequest::Begin(begin) if begin.port() == 80 => {
-            let onion_service_stream = stream_request.accept(Connected::new_empty()).await?;
-            let io = TokioIo::new(onion_service_stream);
+        IncomingStreamRequest::Begin(begin) => match begin.port() {
+            80 => {
+                let onion_service_stream = stream_request.accept(Connected::new_empty()).await?;
+                let io = TokioIo::new(onion_service_stream);
 
-            http1::Builder::new()
-                .serve_connection(io, service_fn(|request| handler.serve(request)))
-                .await?;
-        }
+                http1::Builder::new()
+                    .serve_connection(io, service_fn(|request| handler.serve(request)))
+                    .await?;
+            }
+            1081 => {
+                eprintln!("custom SOCKS5 connection");
+                let mut onion_service_stream =
+                    stream_request.accept(Connected::new_empty()).await?;
+                eprintln!("onion service stream accepted");
+                socks::handle(&mut onion_service_stream).await?;
+                eprintln!("custom SOCKS5 connection finished");
+            }
+            1082 => {
+                eprintln!("ping connection");
+                let mut onion_service_stream =
+                    stream_request.accept(Connected::new_empty()).await?;
+                eprintln!("onion service stream accepted");
+                handle_ping(&mut onion_service_stream).await?;
+                eprintln!("ping connection finished");
+            }
+            _ => {
+                stream_request.shutdown_circuit()?;
+            }
+        },
         _ => {
             stream_request.shutdown_circuit()?;
         }
     }
 
     Ok(())
+}
+
+async fn handle_ping(data_stream: &mut DataStream) -> error::Result<()> {
+    // String buffer to keep data until newline
+    let mut str_buf = String::new();
+    // Infinite loop until the connection is closed
+    loop {
+        // Read a single byte from the client
+        let mut buf = [0u8; 1];
+        data_stream.read_exact(&mut buf).await?;
+        // Store the byte in the string buffer
+        str_buf.push(buf[0] as char);
+        // If the byte is a newline, print the string buffer and clear it
+        if buf[0] == b'\n' {
+            println!("Received: {}", str_buf);
+            if str_buf == "exit\n" {
+                return Ok(());
+            }
+            data_stream.write_all(str_buf.as_bytes()).await?;
+            data_stream.flush().await?;
+            str_buf.clear();
+        }
+    }
 }
